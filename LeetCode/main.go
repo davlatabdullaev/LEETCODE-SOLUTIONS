bc6b80d699cf0b55064e41577cabd790bd73fe5d
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-11 01:37:34 +0500

    leet code

---
+++ LeetCode/main.go
@@ -0,0 +1,11 @@
+package main
+
+import plusone "leetcode/PlusOne"
+
+func main() {
+	// nums := []int{1, 3, 5, 6}
+	// target := 7
+	// searchinsertposition.SearchInsert(nums, target)
+	digits := []int{1,2,3}
+	plusone.PlusOne(digits)
+   }
